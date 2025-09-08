4c8427c149419e10aabf7b621d72470eb1acf20b
Teetad <teetadraseesit@gmail.com>
2025-09-08 11:44:51 +0700

    fixing

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import { Button, Stack, Title, Divider, Container } from "@mantine/core";
-import { v4 as uuidv4 } from "uuid";
-import AddExpenseModal from "../components/Modal"
 
-type Expense = {
+
+/*type Expense = {
   id: string;
   name: string;
   amount: number | string;
   category: string;
-};
+};*/
 
 export default function ExpenseTracker() {
   const [opened, setOpened] = useState(false);
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-  const categories = ["Food", "Transport", "Entertainment"];
+  //const [expenses, setExpenses] = useState<Expense[]>([]);
+  //const categories = ["Food", "Transport", "Entertainment"];
 
   return (
     <Container style={{ maxWidth: 600, margin: "auto", padding: 20 }}>
